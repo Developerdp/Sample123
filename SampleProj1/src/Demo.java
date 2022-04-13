d55ff9f72e9897280a04b7cd0fa7f825b920fd2f
kdp890 <kdp89017@gmail.com>
2022-04-13 12:56:16 +0530

    new programming file added and changes are made on voter and Demo class

--- SampleProj1/src/Demo.java
+++ SampleProj1/src/Demo.java
@@ -4,5 +4,10 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("This program is for practising purpose only");
+	
+	}
+	public static void display()
+	{
+		System.out.println("display() method is created in Demo class");
 	}
 }
--- SampleProj1/src/Voter.java
+++ SampleProj1/src/Voter.java
@@ -6,12 +6,13 @@ public class Voter
 		int age=17;
 		if(age>=18)
 		{
-			System.out.println("you are eligible to vote");
+			System.out.println("you are Age is greater than or equal to 18 you are eligible");
 		}
 		else
 		{
 			System.out.println("you are not eligible to vote");
 		}
+		System.out.println("Changes are made in Voter class");
 			
 		
 	}
