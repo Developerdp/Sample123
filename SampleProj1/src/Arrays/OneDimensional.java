cd0be5ca3c7324dd7a33f2b2136781e1c8a7f7bf
kdp890 <kdp89017@gmail.com>
2022-04-13 11:17:17 +0530

    new changes are made

---
+++ SampleProj1/src/Arrays/OneDimensional.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class OneDimensional 
+{
+	public static void main(String[] args) {
+		int a[] =new int [3];
+	
+		System.out.println("length of the array a: "+a.length);
+	}	
+	
+
+}
